14624e7907d59dac7696a4cc66d8840809c6c5c4
victorolv <victorolvdev@gmail.com>
2025-04-13 22:15:28 -0300

    profile

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import './App.css'
 import Header from './components/header/Header'
+import TextoComSub from './components/textoComSub/TextoComSub'
 
 
 function App() {
   return (
     <div className={`w-screen h-screen bg-bg-black`}>
       <Header />
+      <main className='md:grid grid-cols-[8fr_12fr] py-[8rem]'>
+        <section className='bg-red-400 h-auto'>
+          <div className=''>
+            <h3>aaa</h3>
+          </div>
+        </section>
+        <div className=''>
+          <TextoComSub  principal={"SOFTWARE"} sub={"ENGINEER"}/>
+        </div>
+      </main>
   </div>
   )
 }
---
+++ src/components/textoComSub/TextoComSub.jsx
@@ -0,0 +1,10 @@
+function TextoComSub({principal,sub}) {
+    return (
+    <div className={"px-10"}>
+     <h2 className="text-color-white text-9xl font-extrabold">{principal}</h2>
+     <h2 className="text-color-cinza-2 text-9xl font-extrabold">{sub}</h2>
+    </div>
+    )
+  }
+export default TextoComSub
+  
